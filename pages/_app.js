3a27bcd431530f65263a7abd1e3150681a595991
hadi-rahimii <0hadi.rahimi@gmail.com>
2024-02-24 18:22:13 +0330

    fix footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/modules/Navbar/Navbar";
+import Footer from "@/components/modules/Footer/Footer";
 export default function App({ Component, pageProps }) {
   return(
     <>
     <Navbar />
     <Component {...pageProps} />;
+    <Footer/>
   </>
   );
 }
